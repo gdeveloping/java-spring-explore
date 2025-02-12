a11cb9a7406eb561a7a43c7b1adf22fb736ca71d
gdev <gdeveloping0@gmail.com>
2025-02-12 22:25:47 +0800

    add web-basic-explore

---
+++ web-basic-explore/src/main/java/tech/gdev/Main.java
@@ -0,0 +1,11 @@
+package tech.gdev;
+
+/**
+ * @author gdev
+ * @date 2025/2/12 21:33
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
