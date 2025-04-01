1740e5c7cac5512434c7afb3a8e9722fdea4b44f
gdev <gdeveloping0@gmail.com>
2025-04-01 23:39:05 +0800

    add @MapperScan

--- spring-basic-explore/src/main/java/tech/gdev/springbasicexplore/App.java
+++ spring-basic-explore/src/main/java/tech/gdev/springbasicexplore/App.java
@@ -1,5 +1,6 @@
 package tech.gdev.springbasicexplore;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan(basePackages = "tech.gdev.springbasicexplore",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = AppCodeExplore.class))
 @PropertySource("classpath:application.properties")
+@MapperScan
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
