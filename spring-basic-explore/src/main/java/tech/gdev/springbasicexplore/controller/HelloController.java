a7e7daea6bd473a598a58a70692096a2b406d1d8
gdev <gdeveloping0@gmail.com>
2025-07-13 20:01:54 +0800

    explore controller

--- spring-basic-explore/src/main/java/tech/gdev/springbasicexplore/controller/HelloController.java
+++ spring-basic-explore/src/main/java/tech/gdev/springbasicexplore/controller/HelloController.java
@@ -23,16 +23,36 @@ import java.util.Map;
 @Log4j2
 public class HelloController {
 
-    @GetMapping("/hello1")
-    public Map<String, String> hello(@RequestParam(value = "name", required = false) @Validated @Length(min = 2) String name) {
+    @GetMapping("/hello")
+    public Map<String, String> hello1(@RequestParam(value = "name", required = false) @Validated @Length(min = 2) String name) {
         if (!StringUtils.hasLength(name)) {
             name = "DefaultName";
         }
         return Collections.singletonMap("body", "Hello, " + name);
     }
 
-    @GetMapping("/hello2/{name}")
+    /**
+     * 包含路径参数的 url，例如 `/explore/hello//hello/{name}/morning` 不会被缓存到
+     * `org.springframework.web.servlet.handler.AbstractHandlerMethodMapping.MappingRegistry#pathLookup` 中。
+     */
+    @GetMapping("/hello/{name}/morning")
     public Map<String, String> hello2(@PathVariable(value = "name") @Length(min = 2) String name) {
-        return Collections.singletonMap("body", "Hello, " + name);
+        return Collections.singletonMap("body", "Hello, " + name + " , morning");
+    }
+
+    /**
+     * URL 优先级高于包含通配符的 URL {@link HelloController#morning2()} }
+     */
+    @GetMapping("/morning/{name}")
+    public Map<String, String> morning1(@PathVariable(value = "name") @Length(min = 2) String name) {
+        return Collections.singletonMap("body", "Morning, " + name);
+    }
+
+    /**
+     * 因包含通配符，所以 URL 优先级低于 {@link HelloController#morning1(String)}
+     */
+    @GetMapping("/morning/**")
+    public Map<String, String> morning2() {
+        return Collections.singletonMap("body", "Morning");
     }
 }
