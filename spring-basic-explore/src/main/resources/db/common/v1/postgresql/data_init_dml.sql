f4f5b229f7cf8d24b24cd50a92c7f51a48074db5
gdev <gdeveloping0@gmail.com>
2025-10-04 19:18:46 +0800

    update data

--- spring-basic-explore/src/main/resources/db/common/v1/postgresql/data_init_dml.sql
+++ spring-basic-explore/src/main/resources/db/common/v1/postgresql/data_init_dml.sql
@@ -5,7 +5,7 @@ TRUNCATE "order";
 TRUNCATE "user";
 
 INSERT INTO test_code VALUES (1, 1001, 'test code 1');
-INSERT INTO test_code VALUES (2, 1002, 'test code 2');
+INSERT INTO test_code VALUES (2, 2001, 'test code 2');
 
 INSERT INTO test_user VALUES (1, 'Alice', 'alice@example.com');
 INSERT INTO test_user VALUES (2, 'Bob', 'bob@example.com');
